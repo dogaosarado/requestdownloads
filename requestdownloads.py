492d3ad91763756a2f1521c1c62b3369952e0638
Gustavo Denani <gustavohdenani@gmail.com>
2019-08-27 01:52:54 -0300

    verifica se arquivo ja existe na pasta com isfile da lib OS

--- requestdownloads.py
+++ requestdownloads.py
@@ -3,7 +3,6 @@ import requests
 from time import strptime
 
 main_url = 'http://37.156.146.163/PUB/Pentester_Academy/Pentester_Academy_Web_Application_Pentesting/Pentester_Academy_Web_Application_Pentesting_ITGEEKS/'
-arquivos_pasta = os.listdir('H:\projeto gustavinho proxy')
 print(arquivos_pasta)
 response = request.get(main_url)
 colunas = response.html.find('a')
@@ -13,10 +12,11 @@ extensoes = ('.zip', '.mp4', '.pdf', '.html', '.php')
 # Retorna True caso encontre, False no contrario 
 checar_extensao = lambda link: any((ext in link for ext in extensoes))
 for link in colunas:
-    if checar_extensao(link.full_text):
-        response = request.get(main_url + link.full_text)
-        with open(link.full_text, "wb") as downloaded_file:
-            downloaded_file.write(response.content)
-        print('foi ok um abraço: ', link.full_text)
-        print(strptime())
-print(colunas)
+	if os.path.isfile(link.full_text) == False:
+	    if checar_extensao(link.full_text):
+	        response = request.get(main_url + link.full_text)
+	        with open(link.full_text, "wb") as downloaded_file:
+	            downloaded_file.write(response.content)
+	        print('foi ok um abraço: ', link.full_text)
+	        print(strptime())
+	print(colunas)
